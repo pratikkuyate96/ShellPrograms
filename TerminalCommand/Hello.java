029d763450695b9a225e7c1f9950993e9ecfe4fd
pratikkuyate96 <62277306+pratikkuyate96@users.noreply.github.com>
2020-03-21 19:00:19 +0530

    Hello Java Program

---
+++ TerminalCommand/Hello.java
@@ -0,0 +1,12 @@
+public  class Hello{	
+	public static void main(String[] args){
+		while(true){
+			System.out.println("Hello World");
+				try{
+						Thread.sleep(600000);
+					}catch(Exception e){
+						System.out.println(e);
+					}
+			}
+	}
+}
